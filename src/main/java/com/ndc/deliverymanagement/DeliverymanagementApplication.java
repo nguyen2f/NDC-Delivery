cf66838e3c2bf444d03e088b87c84367bb81a61f
nguyen2f <nguyen.nguyenphuc.dev@gmail.com>
2024-10-08 18:55:11 +0700

    first commit

---
+++ src/main/java/com/ndc/deliverymanagement/DeliverymanagementApplication.java
@@ -0,0 +1,13 @@
+package com.ndc.deliverymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverymanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliverymanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ndc/deliverymanagement/DeliverymanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndc.deliverymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverymanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
